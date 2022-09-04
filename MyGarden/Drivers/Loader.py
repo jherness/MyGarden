b954955a464c2ecd17813f05bd303ae18c944ec4
jherness <jonatanherness@gmail.com>
2022-09-04 10:29:55 +0300

    04/09/2022 -Y

---
+++ MyGarden/Drivers/Loader.py
@@ -0,0 +1,22 @@
+import json
+
+def getdata():
+    return {
+        "key1": 1,
+        "key2": 2,
+        "key3": 3,
+        "key4": 4,
+        "key5": 5,
+        "key6": 6
+    }
+
+
+def main():
+    return getdata()
+
+
+
+if __name__ == '__main__':
+    main()
+
+
