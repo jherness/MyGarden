50912e9825af8503848eb2c4b1e0a87666855f44
jherness <jonatanherness@gmail.com>
2022-09-06 21:14:11 +0300

    06/09/2022 - Y

--- MyGarden/Frontend/Components/PageHead.jsx
+++ MyGarden/Frontend/Components/PageHead.jsx
@@ -1,13 +1,14 @@
 import { View } from 'react-native'
 import { Stack, Button, Flex, Text } from "@react-native-material/core";
 import React from 'react'
+import { mainColor } from '../Style/Colors';
 
 export default function PageHead(props) {
     let first = props.first, second = props.second || ""
   return (
     <View style={{marginTop:50}}>
-      <Text variant="h3">{first}</Text>
-      <Text variant="h3" color="#13a86c" >
+      <Text variant="h3" color={mainColor} >{first}</Text>
+      <Text variant="h3" >
         {second}
       </Text>
     </View>
--- MyGarden/Frontend/Pages/Analytics.js
+++ MyGarden/Frontend/Pages/Analytics.js
@@ -4,41 +4,19 @@ import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 import { Wrap, Box, Divider ,HStack, VStack,TextInput} from "@react-native-material/core";
 import { View, Image, StyleSheet } from 'react-native';
 import Greetings from "../Components/Greetings";
+import PageHead from "../Components/PageHead";
+import { backColor, mainColor } from "../Style/Colors";
 
 
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 50,
-  },
- 
-  logo: {
-    width: 350,
-    height: 250,
-  },
-});
-
-;
 
 const Analythics = ({navigation}) => (
-  <VStack fill center spacing={0} style={{ backgroundColor: "#E5E4D7" }}>
-    <HStack fill center spacing={0}>
+  <VStack fill center spacing={1} style={{ backgroundColor: backColor }}>
+    <HStack fill center spacing={1}>
       <Flex direction="column">
-        <Greetings />
+        <PageHead first="Analytics"/>
       </Flex>
     </HStack>
 
-    <HStack>
-      <Text variant="h6" style={{ marginTop: 90 }}>
-        Analytic
-      </Text>
-      <Text
-        variant="h6"
-        color="aquamarine"
-        style={{ marginTop: 90, marginBottom: 50 }}
-      >
-        Data
-      </Text>
-    </HStack>
     <View style={styles.container}>
       <Image
         style={styles.logo}
@@ -51,5 +29,16 @@ const Analythics = ({navigation}) => (
     <VStack fill center spacing={1}></VStack>
   </VStack>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+ 
+  logo: {
+    width: 350,
+    height: 250,
+  },
+});
 
 export default Analythics;
