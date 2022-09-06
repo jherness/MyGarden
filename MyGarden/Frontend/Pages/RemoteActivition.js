9575af208772e559fb0ce843fb524a8c9d6dfd4d
jherness <jonatanherness@gmail.com>
2022-09-06 11:17:34 +0300

    06/09/2022 - Y

--- MyGarden/Frontend/Pages/RemoteActivition.js
+++ MyGarden/Frontend/Pages/RemoteActivition.js
@@ -1,60 +1,201 @@
-import React from "react";
-import { Stack, Button ,Flex,Text} from "@react-native-material/core";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
-import { Wrap, Box, Divider ,HStack, VStack,TextInput} from "@react-native-material/core";
 import { View, Image, StyleSheet } from 'react-native';
 import Dropdown from'../Components/DropDown';
 import Greetings from "../Components/Greetings";
+import React, { useState } from 'react';
+import { Stack, Button ,Flex,Text,Switch,Avatar  } from "@react-native-material/core";
+import { Wrap, Box, Divider ,HStack, VStack,TextInput} from "@react-native-material/core";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 
 
 
 
-const Remote = () => (
-  <VStack fill center spacing={-40}>
-    <HStack fill center spacing={0}>
-      <Flex direction="column">
-        <Greetings/>
-      </Flex>
+const Remote = () => {
+
+
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
+    console.warn("A date has been picked: ", date);
+    hideDatePicker();
+  };
+ 
+  const [dataToSend , setdataToSend] = useState(data)
+  const [startingData , setStarting] = useState("")
+  const [finishingData , setFinishing] = useState("")
+  const [checkedWater, setCheckedW] = useState(true);
+  const [checkedLight, setCheckedL] = useState(true);
+  const [checkedAir, setCheckedA] = useState(true);
+
+  const data = {startingData:"", finishingData:"", systemToTurn: [true, true,true]};
+
+  
+ 
+const dataChecker=(startingData,finishingData,checkedWater,checkedLight,checkedAir)=>{
+  debugger
+   if (startingData===""||finishingData==="") {
+    alert("You Didnt Fill The Input Right ")
+    
+   } else {
+    data.startingData={startingData}
+    data.finishingData={finishingData}
+    data.systemToTurn=[checkedWater,checkedLight,checkedAir]
+    setdataToSend(data)
+    console.log(data)
+    
+   }
+  
+
+}
+
+
+  
+ 
+ 
+  
+  
+
+  
+
+  return (
+  <VStack fill center spacing={-40} 
+  >
+  <HStack fill center spacing={2} > 
+
+  <Flex direction="column">
+    <Text variant="h4" style={{ margin: 16 }} color="aquamarine" >
+    Greetings
+    </Text>
+     <Text variant="h5" style={{ margin: 16 }}>
+      Nir
+    </Text>
+    
+    
+    </Flex>
+     <Flex direction="Row">
+    <Text variant="h6" style={{ marginBottom: 16 }}
+     >
+    Temp
+    </Text>
+    <Icon size={30} name="temperature-celsius"/>
+    <Icon size={30} name="weather-partly-cloudy"/>
+    
+
+    
+     
+    
+    
+    </Flex>
+    
+  </HStack>
+  <HStack>
+  <Text variant="h6" 
+     >
+    Remote
+    </Text>
+    <Text variant="h6" color="aquamarine"
+    style={{ marginBottom:50}}
+    
+     >
+     Activtion    
+     
+     </Text> </HStack>
+   
+    
+  
+    
+
+
+  
+   <VStack fill center spacing={0.5} style={{marginBottom:30}} >
+   <Button title="Show Date Picker" color="aquamarine" onPress={showDatePicker} />
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+      />
+     <TextInput
+      placeholder="Finishing time"
+      leading={props => <Icon name="clock-plus-outline" {...props} />}
+      onChangeText={newText => setFinishing(newText)}
+      
+    />
+    /**WATER ICON AND SWITCH STACK */
+     <HStack fill center spacing={2} > 
+     <Avatar size={40} icon={props => <Icon name="watering-can" {...props} />}
+   />
+    <Switch value={checkedWater} onValueChange={() => setCheckedW(!checkedWater)} />
+
+    
+     </HStack>
+
+
+
+     /**Light ICON AND SWITCH STACK */
+     <HStack fill center spacing={2} > 
+     <Avatar
+     size={40}
+    
+    icon={props => <Icon name="lightbulb" {...props} />}
+  />
+   <Switch value={checkedLight} onValueChange={() => setCheckedL(!checkedLight)} />
+
+    
     </HStack>
-    <HStack>
-      <Text variant="h6" style={{ marginTop: 90 }}>
-        Remote
-      </Text>
-      <Text
-        variant="h6"
-        color="aquamarine"
-        style={{ marginTop: 90, marginBottom: 50 }}
-      >
-        Activtion
-      </Text>{" "}
+
+
+
+    /**AIR ICON AND SWITCH STACK */
+    <HStack fill center spacing={2} > 
+    <Avatar
+    size={40}
+    
+    icon={props => <Icon name="fan" {...props} />}
+  />
+   <Switch value={checkedAir} onValueChange={() => setCheckedA(!checkedAir)} />
+   
+    
     </HStack>
 
-    <VStack fill center spacing={1}>
-      <TextInput
-        label="Starting Time"
-        leading={(props) => <Icon name="clock-plus-outline" {...props} />}
-      />
-      <TextInput
-        label="Finishing time"
-        leading={(props) => <Icon name="clock-plus-outline" {...props} />}
-      />
-      <Dropdown />
-
-      <Button
-        style={{
-          maxWidth: "100px",
-          maxHeight: "100px",
-          minWidth: "150px",
-          minHeight: "40px",
-          marginTop: "30px",
-        }}
-        color="aquamarine"
-        title="Start Activition"
-        leading={(props) => <Icon name="remote" {...props} />}
-      />
+     <Button
+      style={{
+         maxWidth: "40%",
+          maxHeight: "20%",
+          minWidth: "30%",
+          minHeight: "5%",
+          marginBottom:"20"
+         
+      }}
+      
+      
+      color="aquamarine"
+      
+      title="Start Activition"
+      leading={props => <Icon name="remote" {...props} />}
+      onPress={()=>dataChecker(startingData,finishingData,checkedWater,checkedLight,checkedAir)}
+    />
+     
     </VStack>
-  </VStack>
-);
+
+
+
+
+     
+  
+    
+  </VStack>)
+  
+    ;
+}
 
 export default Remote;
