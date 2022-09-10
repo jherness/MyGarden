dcd2f71eba7a01ad1e73755f1d3201cdcff942e1
jherness <jonatanherness@gmail.com>
2022-09-10 14:54:05 +0300

    10/09/2022 - Y

---
+++ MyGarden/Server/config.js
@@ -0,0 +1,10 @@
+const config = {
+    db: {
+      host: "localhost",
+      user: "root",
+      password: "admin",
+      database: "mygarden",
+    },
+    listPerPage: 10,
+  };
+  module.exports = config;
