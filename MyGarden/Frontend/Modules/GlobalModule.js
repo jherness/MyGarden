4f29aaae1385c8981ddd1f3dae046e29f71ac873
jherness <jonatanherness@gmail.com>
2022-09-10 21:41:25 +0300

    10/09/2022 - Y

--- MyGarden/Frontend/Modules/GlobalModule.js
+++ MyGarden/Frontend/Modules/GlobalModule.js
@@ -1,42 +1,16 @@
-import {User} from '../Classes/User'
+import { User } from "../Classes/User";
 
 export const getHeadlineText = (currentTime) => {
-    let hour = currentTime.getHours()
-    if(hour >= 6 && hour <= 11)
-        return "Good Morning"
-    else if(hour == 12)
-        return "Good Noon"
-    else if(hour >= 13 && hour <= 17)
-        return "Good Afternoon"
-    else
-        return "Good Evening"
+  let hour = currentTime.getHours();
+  if (hour >= 6 && hour <= 11) return "Good Morning";
+  else if (hour == 12) return "Good Noon";
+  else if (hour >= 13 && hour <= 17) return "Good Afternoon";
+  else return "Good Evening";
 };
 
-
 export const getUser = () => {
-    /*TODO: Fetch data from server about the user/check if in local storage first*/
-    return new User();
-}
-
-
-// export const getSamplesFromDB = () => {
-//     fetch(`http://192.168.1.192:3000/samples`)
-//       .then(response => response.json())
-//       .then(samples => console.log(samples))
-// }
-
-
-export const getActivations = () => {
-    fetch(`http://192.168.1.192:3000/activationHistory`)
-      .then(response => response.json())
-      .then(samples => console.log(samples))
-}
-
-
-
-
-
-
-
+  /*TODO: Fetch data from server about the user/check if in local storage first*/
+  return new User();
+};
 
 
--- MyGarden/Frontend/Pages/ActivationHistory.js
+++ MyGarden/Frontend/Pages/ActivationHistory.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from 'react-native'
+import { View, Text } from "react-native";
 import {
   Wrap,
   Box,
@@ -7,17 +7,49 @@ import {
   HStack,
   VStack,
   TextInput,
+  ListItem,
 } from "@react-native-material/core";
 import PageHead from "../Components/PageHead.jsx";
 import { getActivations } from "../Modules/GlobalModule.js";
+import { List } from "react-native-paper";
+import Timeline from "react-native-timeline-flatlist";
 
-export default function ActivationHistory({navigate}) {
-  getActivations()
+export default function ActivationHistory({ navigate }) {
+  const [activations, setActivations] = useState([]);
+  const [timeline, setTimeline] = useState([]);
+
+  const getActivations = async () => {
+    try {
+      const response = await fetch(
+        `http://192.168.1.192:3000/activationHistory`
+      );
+      const data = await response.json();
+      setActivations(data);
+    } catch (err) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getActivations();
+    //get data from DB every 60 seconds
+    const interval = (setInterval(() => {
+      getActivations();
+    }, 60000));
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, []);
+
   return (
     <VStack fill center spacing={1} style={{ backgroundColor: "#E5E4D7" }}>
       <HStack fill center spacing={1}>
-        <PageHead first = "Activation" second="History"/>
+        <PageHead first="Activation" second="History" />
+      </HStack>
+      <HStack fill center>
+        <Timeline data={activations} />
       </HStack>
     </VStack>
-  )
+  );
 }
