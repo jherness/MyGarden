5f864f4e3f02f095213009122f9f930cf8c5815b
jherness <jonatanherness@gmail.com>
2022-09-14 18:02:32 +0300

    14/09/2022 - Y

--- MyGarden/Server/Services/SysMod.js
+++ MyGarden/Server/Services/SysMod.js
@@ -2,23 +2,23 @@ const db = require("./db");
 const helper = require("../helper");
 const config = require("../config");
 
-/*Get latest schedule */
+/*Get SysMod */
 async function getSysMod() {
   const rows = await db.query(`SELECT * FROM sys_mod order by id Desc Limit 1`);
   const data = helper.emptyOrRows(rows);
   return data;
 }
 
-/*Create a new schedule*/
+/*Create a SysMod*/
 async function create(data) {
   const result = await db.query(
      `INSERT INTO sys_mod (is_auto, max_temp, min_moist) values (${data.isActive}, ${data.maxTemp}, ${data.minMoist} )`
   );
 
-  let message = "Error in creating a new remote_activation";
+  let message = "Error in creating a new sys_mod";
 
   if (result.affectedRows) {
-    message = "1 Row Added to remote_activation. Updated currently_active";
+    message = "1 Row Added to sys_mod.";
   }
 
   return { message };
