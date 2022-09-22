54a0a32b2211b8c205cc863b480a5077f99e1fc7
jherness <jonatanherness@gmail.com>
2022-09-22 13:51:11 +0300

    Update ActivationHistory.js

--- MyGarden/Frontend/Pages/ActivationHistory.js
+++ MyGarden/Frontend/Pages/ActivationHistory.js
@@ -6,15 +6,13 @@ import { getActivations } from "../Modules/gets";
 export default function ActivationHistory({ navigate }) {
   const [activations, setActivations] = useState([]);
 
-
   useEffect(() => {
     getActivations(setActivations);
-
   }, []);
 
   return (
     <VStack fill center spacing={1} style={{ backgroundColor: "#E5E4D7" }}>
-      <HStack fill >
+      <HStack fill>
         <Timeline data={activations} />
       </HStack>
     </VStack>
