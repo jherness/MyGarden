172697a3a9da64c20794bd6638dfb2be9214a9a0
jherness <jonatanherness@gmail.com>
2022-09-22 13:33:17 +0300

    22/09/2022 - Y

--- MyGarden/Frontend/Pages/ActivationHistory.js
+++ MyGarden/Frontend/Pages/ActivationHistory.js
@@ -7,22 +7,14 @@ export default function ActivationHistory({ navigate }) {
   const [activations, setActivations] = useState([]);
 
 
-
   useEffect(() => {
     getActivations(setActivations);
-    //get data from DB every 60 seconds
-    const interval = setInterval(() => {
-      getActivations(setActivations);
-    }, 60000);
 
-    return () => {
-      clearInterval(interval);
-    };
   }, []);
 
   return (
     <VStack fill center spacing={1} style={{ backgroundColor: "#E5E4D7" }}>
-      <HStack fill style={{ marginTop: "0%" }}>
+      <HStack fill >
         <Timeline data={activations} />
       </HStack>
     </VStack>
