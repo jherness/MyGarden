9a325d4cd5bf96c3885feab7c9ff1d9ca09ceea5
jherness <jonatanherness@gmail.com>
2022-09-30 01:40:13 +0300

    30/09/2022

---
+++ MyGarden/Hardware/manager/releyscontroller.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append('/home/pi/irrsys/drivers/')
+import pcf8574
+
+
+def main():
+    #pcf8574.activate_water()
+    #pcf8574.deactivate_water()
+    #pcf8574.activate_fan()
+    #pcf8574.deactivate_fan()
+    #pcf8574.activate_light()
+    #pcf8574.deactivate_light()
+    #pcf8574.activate_fertelize()
+    #pcf8574.deactivate_fertelize()
+
+
+if __name__=="__main__":
+    main()
