4c394e50ef90d82f74782b5faf561291b3b74a18
jherness <jonatanherness@gmail.com>
2022-09-30 23:46:02 +0300

    Update handle_auto_mode.py

--- MyGarden/Hardware/manager/handle_auto_mode.py
+++ MyGarden/Hardware/manager/handle_auto_mode.py
@@ -15,83 +15,69 @@ import pcf8574 #relays controller
 import historyLoader
 
 
-
 RELAYS_STATUS = irrmain.get_relays_status()
 SAMPLE = irrmain.getsensordata()
-SYS_MOD = sys_mod.get_sys_mod
+SYS_MOD = sys_mod.get_sys_mod()
 
 
-def check_moist(min_moist, current_moist ):
-    return min_moist>current_moist
+def is_too_dry(min_humid, current_humid ):
+    return current_humid < min_humid 
 
 
-def check_Temp(max_Temp, current_Temp ):
-    return max_Temp<current_Temp
-
-#return True or False about High Temp and Low Humidity activation
-def handle_moist_temp_check(max_temp,current_temp,min_moist,current_moist):
-    return check_moist(min_moist,current_moist) and check_Temp(max_temp,current_temp)
+def is_too_hot(max_temp, current_temp ):
+    return current_Temp > max_temp
 
 
 def handle_manual_mode():
-    current_temp,current_moist = get_sample_params()
-    max_temp,min_moist = get_max_temp_and_min_moist()
-    auto_mode_check=handle_moist_temp_check(max_temp,current_temp,min_moist,current_moist)
+    current_temp, current_humid, current_light = get_sample_params()
+    max_temp, min_humid, max_light = get_sys_mode_params()
     print( "current temp is",current_temp, "max temp is",max_temp)
-    print("current moist is", current_moist, "min moist is" ,min_moist)
+    print("current humid is", current_humid, "min moist is" ,min_humid)
     print("check auto mode ")
     print(auto_mode_check)
     print("__________________________________")
     current_light = get_required_light_from_sample(get_sample())
     print("print check light")
-    print(check_if_to_turn_light(current_light))
+    print(is_dark(current_light))
     print("print check fan")
     print(check_if_turn_fan(current_temp))
     # [0] check Auto mode(Temp and moist)  [1]Dark Or not [2] Hot Or not [3] moist or not
-    return auto_mode_check,check_if_to_turn_light(current_light),check_if_turn_fan(current_temp),check_if_turn_water(current_moist)
+    return auto_mode_check,is_dark(current_light),check_if_turn_fan(current_temp),check_if_turn_water(current_humid)
 
 
-# Get Current Light
-def get_required_light_from_sample(sample_json_to_check):
-    current_light = sample_json_to_check["light"]
-    return current_light
+def is_time_between(start_hour : datetime, end_time : datetime, check_time=None):
+    if start_hour.hour == 0 and start_hour.minute == 0 and start_hour.second == 1:
+        return False
+    check_time = check_time or datetime.datetime.now()
+    return(check_time.time() >= start_hour.time() and check_time.time() <= end_time.time())
 
 
-# get current Temp
-def get_required_temp_from_sample(sample_json_to_check):
-    current_temp = sample_json_to_check["temperature"]
-    return current_temp
+def sample_is_ok(max_temp, min_humid):
+    return not (is_too_hot())
 
 
 # Check if Turn On The Light
-def check_if_to_turn_light(current_light):
-    return current_light<400
+def is_dark(max_light, current_light):
+    return current_light < 400
+    
 
-
-# Check if to turn on fan
-def check_if_turn_fan(current_temp):
-    return current_temp>32
-
-
-def check_if_turn_water(current_moist):
-    return current_moist<25
-
-
-# geting the current temp and moist
+# geting the current temp, humid, light data
 def get_sample_params():
     current_temp = SAMPLE["temperature"]
-    current_moist = SAMPLE["humidity"]
-    return current_temp, current_moist
+    current_humid = SAMPLE["humidity"]
+    current_light = SAMPLE["light"]
+    return current_temp, current_humid, current_light
 
-# ge the max temp and min moist from propertis_json
+# get the max temp, min humid from sys_mod (set in preferences)
 def get_sys_mode_params():
-    max_temp=propertis_json["max_temp"]
-    min_moist = propertis_json["min_moist"]
-    return max_temp,min_moist
+    max_temp = SYS_MOD["max_temp"]
+    min_humid = SYS_MOD["min_moist"]
+    max_light = 400
+    return max_temp, min_humid, max_light
 
 
 #def handle_auto_mode():
-def make_activiton_code(checkers,is_remote,is_schedule):
+def get_appropriate_activiton_code():
     if checkers[0] == True:
         return 1 # High Temp & Low Humidity Active water and fan
     elif checkers[1]==True:
@@ -105,7 +91,7 @@ def make_activiton_code(checkers,is_remote,is_schedule):
     elif checkers[0] == True and checkers[1]==True:
         return 6  # No Light ,High Temp & Low Humidity  active light water& fan
     elif checkers[2] == True and checkers[3]==True:
-        return 7  # No Light  & Low Humidity  Active light and water
+        return 7  # No Light & Low Humidity  Active light and water
     elif checkers[1] == True and checkers[2]==True:
         return 8  # No Light & High Temp' Active light and fan
 
