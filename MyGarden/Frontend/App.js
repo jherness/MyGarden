ded919983f6f2cbb5031aa4a0ae273e53b7ceeeb
jherness <jonatanherness@gmail.com>
2022-10-12 18:05:24 +0300

    12/10/2022 - Y

--- MyGarden/Frontend/App.js
+++ MyGarden/Frontend/App.js
@@ -1,8 +1,54 @@
-import * as React from "react";
+import React, {useEffect, useRef,useState} from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { MainStackNavigator } from "./Navigation/StackNavigator";
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
+
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+  useEffect(() => {
+    registerForPushNotification().then(token=>setExpoPushToken(token)).
+    catch(err => console.log(err));
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, [])
+
+  async function registerForPushNotification(){
+    const {status} = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    console.log("ABCDEFG ",Permissions.NOTIFICATIONS);
+    if (status != "granted") {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      // finalStatus = status;
+    }
+    if (status !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync());
+    return token
+  }
   return (
     <NavigationContainer>
       <MainStackNavigator />
